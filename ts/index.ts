2ace41343520507c07dca54eceb48bee69fb4aaf
Phil Kunz <gitlab@philkunz.com>
2019-08-22 15:23:09 +0200

    fix(core): update

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,6 @@
 import * as plugins from './smartsystem.plugins';
 
 export class Smartsystem {
-  public env = new plugins.smartenv.Smartenv()
+  public env = new plugins.smartenv.Smartenv();
   public cpus = plugins.os.cpus();
 }
